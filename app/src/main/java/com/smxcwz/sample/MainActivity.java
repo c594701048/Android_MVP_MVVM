d984f518c2612fe9cc6e6bc24b1b67f70fa9047f
smxcwz <smxcwz@163.com>
2018-05-22 17:35:22 +0800

    init

--- app/src/main/java/com/smxcwz/sample/MainActivity.java
+++ app/src/main/java/com/smxcwz/sample/MainActivity.java
@@ -1,13 +1,66 @@
 package com.smxcwz.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.smxcwz.frame.base.FrameBaseActivity;
+import com.smxcwz.frame.eventbus.EventCenter;
+import com.smxcwz.frame.netstatus.NetUtils;
+
+public class MainActivity extends FrameBaseActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	protected void getBundleExtras(Bundle extras) {
+
+	}
+
+	@Override
+	protected int getContentViewLayoutID() {
+		return R.layout.activity_main;
+	}
+
+	@Override
+	protected void onEventComming(EventCenter eventCenter) {
+
+	}
+
+	@Override
+	protected View getLoadingTargetView() {
+		return null;
+	}
+
+	@Override
+	protected void initViewsAndEvents() {
+
+	}
+
+	@Override
+	protected void onNetworkConnected(NetUtils.NetType type) {
+
+	}
+
+	@Override
+	protected void onNetworkDisConnected() {
+
+	}
+
+	@Override
+	protected boolean isApplyStatusBarTranslucency() {
+		return false;
+	}
+
+	@Override
+	protected boolean isBindEventBusHere() {
+		return false;
+	}
+
+	@Override
+	protected boolean toggleOverridePendingTransition() {
+		return false;
+	}
+
+	@Override
+	protected TransitionMode getOverridePendingTransitionMode() {
+		return null;
 	}
 }
